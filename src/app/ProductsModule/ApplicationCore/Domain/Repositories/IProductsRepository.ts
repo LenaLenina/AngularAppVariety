ed64b5412634702fadc635bf74335f185b124c7b
buycst01 <buycst01@gmail.com>
2023-09-10 13:49:20 +0300

    deleting not used code

--- src/app/ProductsModule/ApplicationCore/Domain/Repositories/IProductsRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Product } from "../Entities/Product";
-
-export abstract class IProductsRepository {
-    abstract getProducts(): Product[];
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsRepository } from './ProductsModule/Infrastracture/Persistence/Repositories/products-repository';
 import { INumberEqualService } from './NumberEqualModule/ApplicationCore/Services.Abstractions/INumberEqualService';
 import { NumberEqualService } from './NumberEqualModule/ApplicationCore/Services/number-equal.service';
 import { IDistinctArrayService } from './ArrayExtensions/Services.Abstractions/IDistinctService';
@@ -11,12 +10,10 @@ import { DistinctArrayService } from './ArrayExtensions/Services/distinct-array.
 import { NumberIsEven } from './NumberEqualModule/ApplicationCore/Services/NumberIsEven';
 import { NumberIsOdd } from './NumberEqualModule/ApplicationCore/Services/NumberIsOdd';
 import { NumberBeetween3And5 } from './NumberEqualModule/ApplicationCore/Services/NumberBeetween3And5';
-import { IProductsRepository } from './ProductsModule/ApplicationCore/Domain/Repositories/IProductsRepository';
 import { ICounter } from './CounterModule/ApplicationCore/Services.Abstractions/ICounter';
 import { NumberCounterService } from './CounterModule/ApplicationCore/Services/number-counter.service';
 import { INumberRepository } from './NumberModule/ApplicationCore/Domain/Repositories/INumberRepository';
 import { NumberRepository } from './NumberModule/Infrastracture/Persistence/number-repository';
-import { ProductListComponent } from './ProductsModule/UI/Components/product-list-component/product-list.component';
 import { NumberComponent } from './NumberModule/UI/Components/number/number.component';
 import { NUMBER_EQUAL_CONDITIONS } from './InjectionTokens/NUMBER_EQUAL_CONDITIONS';
 
@@ -24,7 +21,6 @@ import { NUMBER_EQUAL_CONDITIONS } from './InjectionTokens/NUMBER_EQUAL_CONDITIO
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
     NumberComponent
   ],
   imports: [
@@ -33,8 +29,6 @@ import { NUMBER_EQUAL_CONDITIONS } from './InjectionTokens/NUMBER_EQUAL_CONDITIO
   ],
   providers: 
   [
-    {provide: IProductsRepository, useClass: ProductsRepository },
-
     {provide: IDistinctArrayService, useClass: DistinctArrayService },
 
     {provide: ICounter, useClass: NumberCounterService },
